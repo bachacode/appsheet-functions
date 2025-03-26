1fc41753d83828f98dddbe9f36d5d258ec84c765
bachacode <85910843+bachacode@users.noreply.github.com>
2025-03-26 19:00:33 -0400

    feat: enhanced TSM_Widget_Base with generic section and control registration methods

--- includes/Widgets/TSM_Widget_Base.php
+++ includes/Widgets/TSM_Widget_Base.php
@@ -2,8 +2,12 @@
 
 namespace TailorSheet_Manager\Widgets;
 
+use TailorSheet_Manager\Widgets\TSM_Control_Builder;
+
 class TSM_Widget_Base extends \Elementor\Widget_Base 
 {
+    private TSM_Control_Builder $control_builder;
+
     public function get_name()
     {
         return 'tsm_widget_base';
@@ -18,6 +22,35 @@ class TSM_Widget_Base extends \Elementor\Widget_Base
     {
         return [ 'tailorsheet' ];
     }
-}
 
-?>
+    /**
+     * Registers a basic tab
+     * @param string $section_id
+     * @param string $label
+     * @param string $tab_type
+     * @param \Closure $controls
+     */
+    protected function register_generic_section($section_id, $label, $tab_type, $controls) {
+        // Sanitize inputs
+        $section_id = sanitize_key($section_id);
+        $label = sanitize_text_field($label);
+        $tab_type = sanitize_key($tab_type);
+
+        $this->start_controls_section(
+            $section_id,
+            [
+                'label' => esc_html__($label, 'tailorsheet-manager'),
+                'tab'   => $tab_type,
+            ]
+        );
+
+        $controls();
+
+        $this->end_controls_section();
+    }
+
+    protected function register_generic_controls($control_id, $parent_selector, $child_selector) {
+        $this->control_builder = new TSM_Control_Builder($this, $control_id, $parent_selector, $child_selector);
+        return $this->control_builder;
+    }
+}
